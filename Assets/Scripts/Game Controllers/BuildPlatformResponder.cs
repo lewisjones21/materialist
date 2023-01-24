4592f020b36380bae14dc508fbeacf347e412317
Lewis Jones <lewisjones21@hotmail.com>
2023-01-24 14:12:07 +0000

    Implement script for enacting different behaviours on different platforms

---
+++ Assets/Scripts/Game Controllers/BuildPlatformResponder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class BuildPlatformResponder : MonoBehaviour
+{
+    [SerializeField]
+    private RuntimePlatform _buildPlatform;
+
+    [SerializeField]
+    private UnityEvent _actionOnBuildPlatform;
+
+    [SerializeField]
+    private UnityEvent _actionOnOtherBuildPlatforms;
+
+    private void Awake()
+    {
+        if (Application.platform == _buildPlatform)
+        {
+            if (_actionOnBuildPlatform != null)
+            {
+                _actionOnBuildPlatform.Invoke();
+            }
+        }
+        else
+        {
+            if (_actionOnOtherBuildPlatforms != null)
+            {
+                _actionOnOtherBuildPlatforms.Invoke();
+            }
+        }
+    }
+}
